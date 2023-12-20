34e22ca21e4bc3895c1cd62c07dea116f33e5412
Jarvis Brown <brownjarvis21@gmail.com>
2023-12-19 22:13:15 -0500

    almost done

--- flask_app/controllers/projects.py
+++ flask_app/controllers/projects.py
@@ -32,7 +32,6 @@ def projects_home():
     
     return render_template("home.html", user=user, projects=projects)
 
-##### May not need to show Render Info and Detail Page
 
 #### Create page #####
 @app.route('/projects/new')
@@ -57,11 +56,9 @@ def delete_project(project_id):
 def save_uploaded_file(file):
     if file and file.filename != '':
         filename = secure_filename(file.filename)
-        # Calculate the relative path to the upload folder based on the current working directory
         upload_folder = os.path.join(app.root_path, 'static', 'upload')
         full_path = os.path.join(upload_folder, filename)
 
-        # Check if the directory exists, if not, create it
         if not os.path.exists(upload_folder):
             os.makedirs(upload_folder)
 
@@ -72,11 +69,8 @@ def save_uploaded_file(file):
 def save_uploaded_image(image):
     if image and image.filename != '':
         filename = secure_filename(image.filename)
-        # Calculate the relative path to the image upload folder based on the current working directory
         upload_folder = os.path.join(app.root_path, 'static', 'upload')
         full_path = os.path.join(upload_folder, filename)
-
-        # Check if the directory exists, if not, create it
         if not os.path.exists(upload_folder):
             os.makedirs(upload_folder)
 
@@ -86,32 +80,29 @@ def save_uploaded_image(image):
 
 @app.route('/projects', methods=['POST'])
 def add_project():
-    # Extract file and image data from request.files
     uploaded_file = request.files.get('file')
     uploaded_image = request.files.get('image')
-
-    # Check if the form data is valid (passing all file data)
     if not Project.is_valid(request.form, request.files):
         return redirect('/projects/new')
 
-    project_data = request.form.to_dict()  # Convert form data to a mutable dictionary
+    project_data = request.form.to_dict()
 
-    # Handle file upload
+
     file_filename = save_uploaded_file(uploaded_file)
     if file_filename:
-        project_data['file'] = file_filename  # Add the file info to the project data
+        project_data['file'] = file_filename
     else:
         flash("No file uploaded.")
 
     # Handle image upload
     image_filename = save_uploaded_image(uploaded_image)
     if image_filename:
-        project_data['image'] = image_filename  # Add the image info to the project data
+        project_data['image'] = image_filename
     else:
         flash("No image uploaded.")
 
     if not file_filename or not image_filename:
-        return redirect('/projects/new')  # Redirect if either file or image upload failed
+        return redirect('/projects/new')
 
     Project.save(project_data)
     return redirect('/projects/home')
@@ -119,17 +110,13 @@ def add_project():
 #### Update Process #####
 @app.route('/projects/update', methods=["POST"])
 def update_project():
-    # Extract file and image data from request.files
     uploaded_file = request.files.get('file')
     uploaded_image = request.files.get('image')
-
-    # Check if the form data is valid (passing all file data)
     if not Project.is_valid(request.form, request.files):
         return redirect(f"/projects/edit/{request.form['id']}")
 
-    project_data = request.form.to_dict()  # Convert form data to a mutable dictionary
+    project_data = request.form.to_dict()
 
-    # Handle file and image updates if provided
     file_filename = save_uploaded_file(uploaded_file)
     if file_filename:
         project_data['file'] = file_filename
